21709a74a9aa2bd653af05bafdacf7f1337ba8bf
Craig Isenor <cisenor1@gmail.com>
2013-01-17 00:38:35 -0800

    done?

--- BottleLab/src/Bottle.java
+++ BottleLab/src/Bottle.java
@@ -3,15 +3,39 @@ public class Bottle {
 	
 	private String bottleName;
 	private int totalVolume;
-	private int currentAmountInBottle;
+	private int currentAmountInBottle = 0;
+	private boolean isEmpty;
+	private boolean isFull;
 	
 	
-	public void bottle(String inName, int size){
+	public Bottle(String inName, int size){
 		bottleName = inName;
 		totalVolume = size;
+		updateBottle();
+		
 		
 	}
-
+	
+	public boolean isItEmpty(){
+		if(currentAmountInBottle < 1){
+			isEmpty = true;
+		}
+		else {
+			isEmpty = false;
+		}
+		return isEmpty;
+	}
+	
+	public boolean isItFull(){
+		if(currentAmountInBottle >= totalVolume){
+			isFull = true;
+		}
+		else {
+			isFull = false;
+		}
+		return isFull;
+	}
+	
 
 	public String getBottleName() {
 		return bottleName;
@@ -26,5 +50,31 @@ public class Bottle {
 	public int getCurrentAmountInBottle() {
 		return currentAmountInBottle;
 	}
-
+	
+	public void addWaterToBottle(int howMuch){
+		for(int i = 0; i < howMuch; i++){
+			if(! isItFull()){
+				currentAmountInBottle++;
+			}
+		}
+		updateBottle();
+		
+	}
+	
+	public void pourWaterOut(int howMuch){
+		for(int i = 0; i < howMuch; i++){
+			if(! isItEmpty()){
+			currentAmountInBottle--;
+			}
+		}
+		if(currentAmountInBottle < 1){
+			currentAmountInBottle = 0;
+		}
+		updateBottle();
+	}
+	
+	public void updateBottle(){
+		isItFull();
+		isItEmpty();
+	}
 }
--- BottleLab/src/BottleDriver.java
+++ BottleLab/src/BottleDriver.java
@@ -4,17 +4,32 @@ public class BottleDriver {
 	public static void main(String[] args) {
 		
 		// Make a new bottle with name
+		Bottle aBottle = new Bottle("Aquafina", 1000);
+		
 		
 		// Find out how much water is in it
+		aBottle.getCurrentAmountInBottle();
 		
 		// Add water to it
 		
+		aBottle.addWaterToBottle(300);
+		aBottle.addWaterToBottle(300);
+		aBottle.addWaterToBottle(300);
+		aBottle.addWaterToBottle(300);
+		
 		// Pour some out
+		aBottle.pourWaterOut(200);
+		aBottle.pourWaterOut(200);
+		aBottle.pourWaterOut(200);
+		aBottle.pourWaterOut(2000);
+		aBottle.pourWaterOut(200);
 		
 		// Pour until empty
+		//aBottle.pourWaterOut(1000);
 		
 		// Fill until full
-
+		//aBottle.addWaterToBottle(1200);
+		
 	}
 
 }
