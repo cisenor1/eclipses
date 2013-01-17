d8a05373e8068bd5cf43914b9e312f8b51022c3d
Craig Isenor <cisenor1@gmail.com>
2013-01-16 23:26:51 -0800

    as before

---
+++ BottleLab/src/BottleDriver.java
@@ -0,0 +1,20 @@
+
+public class BottleDriver {
+	
+	public static void main(String[] args) {
+		
+		// Make a new bottle with name
+		
+		// Find out how much water is in it
+		
+		// Add water to it
+		
+		// Pour some out
+		
+		// Pour until empty
+		
+		// Fill until full
+
+	}
+
+}
